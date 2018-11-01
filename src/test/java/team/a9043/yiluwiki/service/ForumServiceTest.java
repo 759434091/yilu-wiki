d685cb95175fa94a219cda12885ee729ed830802
a9043 <zzz13129180808@gmail.com>
2018-11-01 20:03:53 +0800

    分页

--- src/test/java/team/a9043/yiluwiki/service/ForumServiceTest.java
+++ src/test/java/team/a9043/yiluwiki/service/ForumServiceTest.java
@@ -1,15 +1,15 @@
 package team.a9043.yiluwiki.service;
 
+import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import team.a9043.yiluwiki.entity.YwForumReply;
+import team.a9043.yiluwiki.entity.YwForumPost;
 import team.a9043.yiluwiki.exception.InvalidParameterException;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,7 +20,7 @@ public class ForumServiceTest {
 
     @Test
     public void test() throws InvalidParameterException {
-        List<YwForumReply> list = forumService.getReplies(10, 1, 10);
+        PageInfo<YwForumPost> list = forumService.getPosts(1, 10);
         log.info(list.toString());
     }
 }
